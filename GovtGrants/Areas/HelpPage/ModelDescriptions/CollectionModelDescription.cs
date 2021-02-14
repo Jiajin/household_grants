56e1cbea184e74ed02fd005766d27e20c1ff5320
Jiajin <backup_sjj@hotmail.com>
2021-02-15 00:46:18 +0800

    Removal of template code
    Removal of some of VS's API template code/files

--- GovtGrants/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace GovtGrants.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- GovtGrants/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace GovtGrants.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
